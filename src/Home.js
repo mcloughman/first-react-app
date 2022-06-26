d56ac2b058ab31208d1173bbce0622251a6af98a
Michael Loughman <mcloughman@gmail.com>
2022-06-26 11:56:06 -0700

    Create isPending state and add it to Home component. We display a Loading message while isPending is true. Surround our fetch request with a setTimeout and use a one second delay to simulate a fetch request to a real db.

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,24 @@ import HikeList from "./HikeList";
 
 const Home = () => {
   const [hikes, setHikes] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   // be careful of changinging state inside useEffect, you could cause an infinite loop
   useEffect(() => {
-    fetch("http://localhost:8000/hikes")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setHikes(data);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8000/hikes")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setHikes(data);
+          setIsPending(false);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {hikes && <HikeList hikes={hikes} title="All Hikes" />}
       {/*  Below is how you would filter the hike list using filter
       <HikeList
