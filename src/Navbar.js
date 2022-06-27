4a591f55ef126d9975ab60d2543d60160aa8a087
Michael Loughman <mcloughman@gmail.com>
2022-06-26 20:15:36 -0700

    Add Create Component and continue implementing routes and links functionality in order ro optimize performance of react-router-dom. These implementations ensure that our app can handle multiple pages without needing to access the server.

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>The Hiking Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create">Add New Hike</a>
+        <Link to="/">Home</Link>
+        <Link to="/create">Add New Hike</Link>
       </div>
     </nav>
   );
