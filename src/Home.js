69699c872c131442edf42b847490530128e39a2e
Michael Loughman <mcloughman@gmail.com>
2022-06-26 17:50:07 -0700

    Add error handling to Home component

--- src/Home.js
+++ src/Home.js
@@ -4,25 +4,37 @@ import HikeList from "./HikeList";
 const Home = () => {
   const [hikes, setHikes] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   // be careful of changinging state inside useEffect, you could cause an infinite loop
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/hikes")
-        .then((response) => response.json())
+        .then((response) => {
+          if (!response.ok) {
+            throw Error("OH NO ERROR!!!");
+          }
+          return response.json();
+        })
         .then((data) => {
-          console.log(data);
-          setHikes(data);
           setIsPending(false);
+          setHikes(data);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
         });
     }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+
       {isPending && <div>Loading...</div>}
       {hikes && <HikeList hikes={hikes} title="All Hikes" />}
-      {/*  Below is how you would filter the hike list using filter
+      {/* Below is how you would filter the hike list using filter
       <HikeList
         hikes={hikes.filter((hike) => hike.author === "Jeffery")}
         title="Jeffery's Blogs"
