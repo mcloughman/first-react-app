857e33e5fed5b5d08a3d89daf660202eecb80917
Michael Loughman <mcloughman@gmail.com>
2022-06-27 18:54:07 -0700

    Add delete button and functionality to HikeDetails component and again import and implement useHistory hook to redirect user back to home page

--- src/HikeDetails.js
+++ src/HikeDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const HikeDetails = () => {
@@ -8,6 +8,17 @@ const HikeDetails = () => {
     isPending,
     error,
   } = useFetch("http://localhost:8000/hikes/" + id);
+
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch("http://localhost:8000/hikes/" + hike.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
+
   return (
     <div className="hike-details">
       {isPending && <div>Loading...</div>}
@@ -22,6 +33,7 @@ const HikeDetails = () => {
             alt={`${hike.title}`}
           />
           <p>{hike.body}</p>
+          <button onClick={handleClick}>Delete Hike</button>
         </article>
       )}
     </div>
